01e3e9cfa294d5f8f88e156344fdf3e945b26d00
root <root@ip-10-136-14-250.ap-southeast-1.compute.internal>
2012-02-15 06:59:23 +0000

    skip private

--- lib/default_watcher/hooks.rb
+++ lib/default_watcher/hooks.rb
@@ -1,6 +1,7 @@
 module DefaultWatcher
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_before_save(context={})
+      return if context[:issue].is_private?
       context[:issue].project.users.all.each do |u| 
         next unless u.allowed_to?(:default_watcher,context[:issue].project)
         context[:issue].add_watcher(u) unless context[:issue].watched_by?(u)
