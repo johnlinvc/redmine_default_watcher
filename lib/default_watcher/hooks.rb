36eb4abae3fedcb5861da0278b2291ccaac35afa
root <root@ip-10-136-14-250.ap-southeast-1.compute.internal>
2012-02-15 06:29:58 +0000

    notify projest users only

--- lib/default_watcher/hooks.rb
+++ lib/default_watcher/hooks.rb
@@ -1,7 +1,7 @@
 module DefaultWatcher
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_before_save(context={})
-      User.all.each do |u| 
+      context[:issue].project.users.all.each do |u| 
         next unless u.allowed_to?(:default_watcher,context[:issue].project)
         context[:issue].add_watcher(u) unless context[:issue].watched_by?(u)
       end
